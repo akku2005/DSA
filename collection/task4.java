e7b9e75d717c60b35ab433df37799dc63f709249
akku2005 <asakashsahu20@gmail.com>
2024-09-20 10:13:47 +0530

    Queue practice question done

---
+++ collection/task4.java
@@ -0,0 +1,17 @@
+package collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class task4 {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("One");
+        queue.add("Two");
+        queue.add("Three");
+        System.out.println("Queue Elements are");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
+}
